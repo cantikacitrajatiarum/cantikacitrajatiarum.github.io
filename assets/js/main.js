61a3131e26d585b096dff6f0435adaef03440d11
Cantika Citrajatiarum <30848259+cantikacitrajatiarum@users.noreply.github.com>
2018-11-08 18:57:36 +1100

    test

---
+++ assets/js/main.js
@@ -0,0 +1,81 @@
+$(document).ready(function(){
+
+// menu slide
+	$('.menu-button').click(function() {
+ 		$('.menu').toggleClass('showme');
+	});
+
+	$('.essay-button').click(function() {
+ 		$('.essay').toggleClass('showme');
+	});
+
+
+// pop up
+	$('.p-1 .p-close').click(function() {
+		$('.p-1').removeClass('p-show');
+	});
+	$('.ns-1').click(function() {
+		$('.p-1').addClass('p-show');
+	});
+
+	$('.p-2 .p-close').click(function() {
+		$('.p-2').removeClass('p-show');
+	});
+	$('.ns-2').click(function() {
+		$('.p-2').addClass('p-show');
+	});
+
+	$('.p-3 .p-close').click(function() {
+		$('.p-3').removeClass('p-show');
+	});
+	$('.ns-3').click(function() {
+		$('.p-3').addClass('p-show');
+	});
+
+	$('.p-4 .p-close').click(function() {
+		$('.p-4').removeClass('p-show');
+	});
+	$('.ns-4').click(function() {
+		$('.p-4').addClass('p-show');
+	});
+
+	$('.p-5 .p-close').click(function() {
+		$('.p-5').removeClass('p-show');
+	});
+	$('.ns-5').click(function() {
+		$('.p-5').addClass('p-show');
+	});
+
+	$('.p-6 .p-close').click(function() {
+		$('.p-6').removeClass('p-show');
+	});
+	$('.ns-6').click(function() {
+		$('.p-6').addClass('p-show');
+	});
+
+	$('.p-7 .p-close').click(function() {
+		$('.p-7').removeClass('p-show');
+	});
+	$('.ns-7').click(function() {
+		$('.p-7').addClass('p-show');
+	});
+
+	$('.p-8 .p-close').click(function() {
+		$('.p-8').removeClass('p-show');
+	});
+	$('.ns-8').click(function() {
+		$('.p-8').addClass('p-show');
+	});
+
+	$('.p-9 .p-close').click(function() {
+		$('.p-9').removeClass('p-show');
+	});
+	$('.ns-9').click(function() {
+		$('.p-9').addClass('p-show');
+	});
+
+
+
+
+
+});
